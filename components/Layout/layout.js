e05b9634a72e8c94228fd55bb8a55305560dc2fd
Florian Winkler <flo@wnklr.art>
2022-06-20 13:04:32 +0200

    Add letter spacing for live count numbers

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,13 +1,13 @@
-import Footer from "../Footer/footer";
-import styles from "./layout.module.scss";
-import Header from "../Header/header";
+import Footer from '../Footer/footer';
+import styles from './layout.module.scss';
+import Header from '../Header/header';
 
 export default function Layout({ children }) {
-  return (
-    <>
-      <Header />
-      <main className={styles.main}>{children}</main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main className={styles.main}>{children}</main>
+			<Footer />
+		</>
+	);
 }
